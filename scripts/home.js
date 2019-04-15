17ca2cfe5128aa4e9db7b9057e6c26a966119489
Callum-A <callumanderson745@gmail.com>
2019-04-15 13:59:51 +0100

    Initial Publish

---
+++ scripts/home.js
@@ -0,0 +1,16 @@
+function hideNav(){
+
+    var navBar = document.getElementById("navBar");
+    if(navBar.style.display === "none"){
+
+        navBar.style.display = "block";
+        document.getElementById("hideNav").innerHTML = "Hide Navigation Bar";
+
+    }else{
+
+        navBar.style.display = "none";
+        document.getElementById("hideNav").innerHTML = "Show Navigation Bar";
+
+    }
+
+}
