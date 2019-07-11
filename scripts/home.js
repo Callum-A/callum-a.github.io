50fb11499824ab095fab8a083165bd2785b78828
Callum Anderson <callumanderson745@gmail.com>
2019-07-11 19:28:32 +0100

    Added projects section

--- scripts/home.js
+++ scripts/home.js
@@ -1,16 +1,15 @@
-function hideNav(){
-
+function hideNav() {
     var navBar = document.getElementById("navBar");
     if(navBar.style.display === "none"){
-
         navBar.style.display = "block";
         document.getElementById("hideNav").innerHTML = "Hide Navigation Bar";
-
-    }else{
-
+    }else {
         navBar.style.display = "none";
         document.getElementById("hideNav").innerHTML = "Show Navigation Bar";
-
     }
 
+}
+
+function openInNewTab(href) {
+    window.open(href);
 }
