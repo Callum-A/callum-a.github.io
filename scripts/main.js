188d5769969e9402888e9b76f0612caed88f1eb9
Callum Anderson <callumanderson745@gmail.com>
2019-07-27 23:28:18 +0100

    Rewrote using Tailwind and made responsive

--- scripts/main.js
+++ scripts/main.js
@@ -1,5 +1,5 @@
 var i = 0;
-var textOptions = ["System.out.println(\"Hey, I'm Callum\");", "Hi I'm Callum and this is my website.", "Hey there I'm Callum."];
+var textOptions = ["I'm Callum, welcome to my website.", "System.out.println(\"Hey, I'm Callum\");", "Hey there I'm Callum."];
 var speed = 100;
 txt = textOptions[Math.floor(Math.random() * textOptions.length)];
 window.onload = function() {
